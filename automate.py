5a7e10613562cffdbeef465cca766e09e5348131
jadams95 <adamsjt95@gmail.com>
2019-06-30 18:21:59 -0400

    upload

--- automate.py
+++ automate.py
@@ -8,10 +8,11 @@ ip = ""
 
 def automation(ip):
     """ Receives the ip address and commands, and then does the recon to an output file """
-    os.system('echo ------------------------------------------------ >> "C:\\BugBounty\\test2.txt"')
-    os.system('nmap.exe -sT -p- -oN c:\\BugBounty\\test2.txt ' + ip)
-    os.system('echo ------------------------------------------------ >> "C:\\BugBounty\\test2.txt"')
-    os.system(r'C:\nikto\nikto.bat -output C:\\BugBounty\\test2.txt -host ' + ip)
+    commands = ['nmap.exe -sT -p- -oN c:\\BugBounty\\test3.txt ',
+    r'C:\nikto\nikto.bat -output C:\\BugBounty\\test3.txt -host ']
+    for command in commands:
+        os.system(command + ip)
+    
     
     
 automation("216.18.168.66")
